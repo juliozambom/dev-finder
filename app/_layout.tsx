a8367a6b1db163eb94750034afe78ae3d6bed855
juliozambom <work.juliozambom@gmail.com>
2024-12-03 12:27:59 -0300

    feat(root-layout): add project fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,24 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    LatoRegular: require('../assets/fonts/Lato-Regular.ttf'),
+    LatoBold: require('../assets/fonts/Lato-Bold.ttf'),
+    BlackHanSans: require('../assets/fonts/BlackHanSans-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <Stack 
       screenOptions={{
