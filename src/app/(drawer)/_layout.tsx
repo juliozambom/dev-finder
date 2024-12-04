2714d2b310361ad3b2098a8502104779f2d8902c
juliozambom <work.juliozambom@gmail.com>
2024-12-04 01:46:40 -0300

    refactor(i18n): move language and color scheme management from DrawerLayout to Index component

--- src/app/(drawer)/_layout.tsx
+++ src/app/(drawer)/_layout.tsx
@@ -1,33 +1,7 @@
 import HomeDrawer from '@/src/modules/home/drawer';
-import { setLanguage } from '@/src/store/app/slice';
-import { useAppDispatch } from '@/src/store/hooks/useAppDispatch';
-import { I18NLanguage } from '@/src/utils/i18n';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Drawer } from 'expo-router/drawer';
-import { useColorScheme } from 'nativewind';
-import { useEffect } from 'react';
 
 export default function DrawerLayout() {
-  const dispatch = useAppDispatch();
-  const { setColorScheme } = useColorScheme();
-
-  useEffect(() => {
-    (async () => {
-      const language = (await AsyncStorage.getItem('language')) as I18NLanguage;
-      const colorScheme = (await AsyncStorage.getItem('ColorScheme')) as
-        | 'light'
-        | 'dark';
-
-      if (colorScheme) {
-        setColorScheme(colorScheme);
-      }
-
-      if (language) {
-        dispatch(setLanguage(language));
-      }
-    })();
-  }, []);
-
   return (
     <Drawer
       defaultStatus="closed"
--- src/app/(drawer)/index.tsx
+++ src/app/(drawer)/index.tsx
@@ -1,5 +1,31 @@
 import Home from '@/src/screens/home';
+import { setLanguage } from '@/src/store/app/slice';
+import { useAppDispatch } from '@/src/store/hooks/useAppDispatch';
+import { I18NLanguage } from '@/src/utils/i18n';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useColorScheme } from 'nativewind';
+import { useEffect } from 'react';
 
 export default function Index() {
+  const dispatch = useAppDispatch();
+  const { setColorScheme } = useColorScheme();
+
+  useEffect(() => {
+    (async () => {
+      const language = (await AsyncStorage.getItem('language')) as I18NLanguage;
+      const colorScheme = (await AsyncStorage.getItem('ColorScheme')) as
+        | 'light'
+        | 'dark';
+
+      if (colorScheme) {
+        setColorScheme(colorScheme);
+      }
+
+      if (language) {
+        dispatch(setLanguage(language));
+      }
+    })();
+  }, []);
+
   return <Home />;
 }
